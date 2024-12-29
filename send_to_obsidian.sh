21ea8b82b70db8d7ff2fda8d8cdaa04c51c7bd7a
Chrispy <chrispitzer@gmail.com>
2024-12-29 15:18:11 -0800

    added bash script to copy files over to obsidian

---
+++ send_to_obsidian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp main.js /mnt/c/Users/chris/Documents/Obsidian/.obsidian/plugins/chrispy-gtd/main.js
+cp manifest.json /mnt/c/Users/chris/Documents/Obsidian/.obsidian/plugins/chrispy-gtd/manifest.json
+
