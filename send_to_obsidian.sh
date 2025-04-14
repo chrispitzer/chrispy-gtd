c587805a380477906b48b142135295242f1eeed4
Chrispy <chrispitzer@gmail.com>
2025-04-13 19:10:42 -0700

    build in watch function

--- send_to_obsidian.sh
+++ send_to_obsidian.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# build the project
+npm run build
+
+# copy the built files to the obsidian plugins directory
 cp main.js /mnt/c/Users/chris/Documents/Obsidian/.obsidian/plugins/chrispy-gtd/main.js
 cp manifest.json /mnt/c/Users/chris/Documents/Obsidian/.obsidian/plugins/chrispy-gtd/manifest.json
 
